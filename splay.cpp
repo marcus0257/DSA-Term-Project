eda9282533f9f86020d5185099a5d6a31e4c892a
marcus0257 <113317592+marcus0257@users.noreply.github.com>
2023-11-15 14:54:38 -0500

    Update splay.cpp

--- splay.cpp
+++ splay.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <unordered_map>
+#include <queue>
 
+class SplayTreeNode {
+public:
+    int key;
+    SplayTreeNode* left;
+    SplayTreeNode* right;
+
+    SplayTreeNode(int value) : key(value), left(nullptr), right(nullptr) {}
+};
+
+class SplayTree {
+public:
+    SplayTreeNode* root;
+
+};
+
+class TrieNode {
+};
+
+class AutocompleteSystem {
+
+};

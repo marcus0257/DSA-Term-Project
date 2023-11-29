518a6bafd7b0c4d6877375f949f34942a39ce85e
AngelJC03 <113469032+AngelJC03@users.noreply.github.com>
2023-11-28 19:29:47 -0500

    Updated main.cpp
    if theres any linker errors its the #include "splay.h" at the top of the page of one of the files

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "splay.h"
 
+int main()
+{
+    SplayTree st;
+    SplayNode* root = nullptr;
+    std::string phrase = "The quick brown fox jumps over the lazy dog";
+
+    st.insert(phrase, root);
+
+    std::cout << "\n";
+    return 0;
+}

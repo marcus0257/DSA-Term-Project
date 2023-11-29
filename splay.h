c715c5a8d1ed72863ea098a2c77a86c34dfcfec8
AngelJC03 <113469032+AngelJC03@users.noreply.github.com>
2023-11-28 19:26:43 -0500

    Updated splay.h
    optimized suboptimal code for optimization

--- splay.h
+++ splay.h
@@ -1,1 +1,38 @@
+#pragma once
 
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+struct SplayNode // node declaration
+{
+    std::string key;
+    SplayNode* left;
+    SplayNode* right;
+};
+
+class SplayTree
+{
+public:
+    SplayNode* RR_Rotate(SplayNode* k2);
+
+    SplayNode* LL_Rotate(SplayNode* k2);
+
+    SplayNode* Splay(const std::string& key, SplayNode* root);
+
+    SplayNode* New_Node(const std::string& key);
+
+    SplayNode* insert(const std::string& key, SplayNode* root);
+
+    SplayNode* Delete(const std::string& key, SplayNode* root);
+
+    SplayNode* Search(const std::string& key, SplayNode* root);
+
+    void InOrder(SplayNode* root);
+
+    void PreOrder(SplayNode* root);
+
+    void PostOrder(SplayNode* root);
+
+};

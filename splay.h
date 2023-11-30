d8747bed00c9b3ee5fc508212fd222abe807aada
AngelJC03 <113469032+AngelJC03@users.noreply.github.com>
2023-11-30 14:50:29 -0500

    Update splay.h
    optimized suboptimal code for optimization optimus prime

--- splay.h
+++ splay.h
@@ -15,24 +15,34 @@ struct SplayNode // node declaration
 class SplayTree
 {
 public:
+    //right rotation
     SplayNode* RR_Rotate(SplayNode* k2);
 
+    //left rotation
     SplayNode* LL_Rotate(SplayNode* k2);
 
+    //splay, bring given value to the root of the tree
     SplayNode* Splay(const std::string& key, SplayNode* root);
 
+    //creates new node
     SplayNode* New_Node(const std::string& key);
 
+    //inserts node with given key
     SplayNode* insert(const std::string& key, SplayNode* root);
 
+    //deletes node with given key
     SplayNode* Delete(const std::string& key, SplayNode* root);
 
+    //returns the given key at the root of the tree
     SplayNode* Search(const std::string& key, SplayNode* root);
 
+    //prints inorder traversal
     void InOrder(SplayNode* root);
 
+    //prints preorder traversal
     void PreOrder(SplayNode* root);
 
+    //prints postorder traversal
     void PostOrder(SplayNode* root);
 
 };

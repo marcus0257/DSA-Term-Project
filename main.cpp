540b22dd25a0c99b3db4a001eaf10d896d564bd5
AngelJC03 <113469032+AngelJC03@users.noreply.github.com>
2023-11-30 14:51:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 
 int main()
 {
+    //example of how the last key inserted is closer to the root of the tree
     SplayTree st;
     SplayNode* root = nullptr;
     std::string phrase = "The quick brown fox jumps over the lazy dog";
-
+    std::string phrase = "Where is the nearest hospital";
+    std::string phrase = "who is the president";
+    std::string phrase = "youtube";
+    std::string phrase = "youtube.com";
+    std::string phrase = "google.com";
+    
     st.insert(phrase, root);
+    
 
     return 0;
 }

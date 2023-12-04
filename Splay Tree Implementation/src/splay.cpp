01cfac3babbf857110dda75d6b75efd190656e54
AngelJC03 <113469032+AngelJC03@users.noreply.github.com>
2023-12-04 18:37:02 -0500

    Update splay.cpp
    drugs

--- Splay Tree Implementation/src/splay.cpp
+++ Splay Tree Implementation/src/splay.cpp
@@ -3,32 +3,19 @@
 
 using namespace std;
 
-// Default constructor for node
-SplayNode::SplayNode() {
-    this->left = nullptr;
-    this->right = nullptr;
-}
-
 // Constructor for a node that takes a key
-SplayNode::SplayNode(int key) {
+SplayNode::SplayNode(std::string key) {
     this->key = key;
     this->left = nullptr;
     this->right = nullptr;
 }
 
-// Default destructor for node
-SplayNode::~SplayNode() {
-
-}
-
-// Default tree constructor
-SplayTree::SplayTree() {
-    this->root = nullptr;
-}
-
-// Tree destructor
-SplayTree::~SplayTree() {
-    delete this->root;
+// Method for creating a new node
+SplayNode* SplayTree::New_Node(std::string key) {
+    // Creating a temp node with two empty children
+    SplayNode* p_node = new SplayNode(key);
+    p_node->left = p_node->right = nullptr;
+    return p_node;
 }
 
 // Method for rotating a node to the right
@@ -48,7 +35,7 @@ SplayNode* SplayTree::rotateLeft(SplayNode* OldKey) {
 }
 
 // Method for splaying a node to the root
-SplayNode* SplayTree::splay(int key, SplayNode* root) {
+SplayNode* SplayTree::splay(std::string key, SplayNode* root) {
     // If the tree is empty, return nullptr
     if (!root) {
         return nullptr;
@@ -104,16 +91,8 @@ SplayNode* SplayTree::splay(int key, SplayNode* root) {
     return root;
 }
 
-SplayNode* SplayTree::New_Node(int key) {
-    // Creating a temp node with two empty children
-    SplayNode* p_node = new SplayNode;
-    p_node->key = key;
-    p_node->left = p_node->right = nullptr;
-    return p_node;
-}
-
 // Method for inserting a node into the tree
-SplayNode* SplayTree::insert(int key, SplayNode* root) {
+SplayNode* SplayTree::insert(std::string key, SplayNode* root) {
     // Creating a new temporary variable
     SplayNode* temp = nullptr;
 
@@ -156,7 +135,8 @@ SplayNode* SplayTree::insert(int key, SplayNode* root) {
     return root;
 }
 
-SplayNode* SplayTree::remove(int key, SplayNode* root) {
+// Method for removing a node from the tree
+SplayNode* SplayTree::remove(std::string key, SplayNode* root) {
     // Check if the current root is null (base case for recursion)
     if (!root) {
         return nullptr;
@@ -216,102 +196,16 @@ SplayNode* SplayTree::remove(int key, SplayNode* root) {
 }
 
 // Recursive call
-void SplayTree::insert(int key) {
+void SplayTree::insert(std::string key) {
     this->root = this->insert(key, this->root);
 }
 
 // Recursive call
-void SplayTree::remove(int key) {
+void SplayTree::remove(std::string key) {
     this->root = this->remove(key, this->root);
 }
 
 // Recursive call
-void SplayTree::preorder() {
-    this->preorder(this->root);
-    cout << '\n';
-}
-
-// Recursive call
-void SplayTree::inorder() {
-    this->inorder(this->root);
-    cout << '\n';
-}
-
-// Recursive call
-void SplayTree::postorder() {
-    this->postorder(this->root);
-    cout << '\n';
-}
-
-// Recursive call
-SplayNode* SplayTree::search(int key) {
+SplayNode* SplayTree::search(std::string key) {
     return this->search(key, this->root);
-}
-
-// Preorder traversal method, prints tree from top to bottom
-void SplayTree::preorder(SplayNode* root) {
-    if (!root) {
-        return;
-    }
-
-    std::cout << root->key << " ";
-    if (root->left) {
-        std::cout << "left: " << root->left->key << " ";
-    }
-    if (root->right) {
-        std::cout << "right: " << root->right->key << " ";
-    }
-    std::cout << '\n';
-    this->preorder(root->left);
-    this->preorder(root->right);
-}
-
-// Prints tree from left to right
-void SplayTree::inorder(SplayNode* root) {
-    if (!root) {
-        return;
-    }
-
-    this->inorder(root->left);
-    std::cout << root->key << " ";
-    if (root->left) {
-        std::cout << "left: " << root->left->key << " ";
-    }
-    if (root->right) {
-        std::cout << "right: " << root->right->key << " ";
-    }
-    std::cout << '\n';
-    this->inorder(root->right);
-}
-
-// Prints tree from right to left
-void SplayTree::postorder(SplayNode* root) {
-    if (!root) {
-        return;
-    }
-
-    this->postorder(root->left);
-    this->postorder(root->right);
-    std::cout << root->key << " ";
-    if (root->left) {
-        std::cout << "left: " << root->left->key << " ";
-    }
-    if (root->right) {
-        std::cout << "right: " << root->right->key << " ";
-    }
-    std::cout << '\n';
-}
-
-// Find out if an item is in the tree, if it is then splay it closer to the root of the tree
-SplayNode* SplayTree::search(int key, SplayNode* root) {
-    // Perform splay operation on the tree rooted at 'root' with the given key
-    root = splay(key, root);
-
-    // Update the root pointer in the class to reflect the new root after splaying
-    this->root = root;
-
-    this->root->color = 1;
-
-    // Now, you can access the root node and perform any other necessary operations
-    return root;
 }

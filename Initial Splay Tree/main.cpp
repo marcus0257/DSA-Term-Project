136169dafabb6ba0366c9f82f68293e4b9fd71ec
AngelJC03 <113469032+AngelJC03@users.noreply.github.com>
2023-12-04 18:31:26 -0500

    Update main.cpp
    optimized

--- Initial Splay Tree/main.cpp
+++ Initial Splay Tree/main.cpp
@@ -3,18 +3,23 @@
 
 int main()
 {
-    //example of how the last key inserted is closer to the root of the tree
-    SplayTree st;
-    SplayNode* root = nullptr;
-    std::string phrase = "The quick brown fox jumps over the lazy dog";
-    std::string phrase = "Where is the nearest hospital";
-    std::string phrase = "who is the president";
-    std::string phrase = "youtube";
-    std::string phrase = "youtube.com";
-    std::string phrase = "google.com";
-    
-    st.insert(phrase, root);
-    
+    SplayTree tree;
+    int phrase = 1;
+    int phrase1 = 2;
+    int phrase2 = 3;
+    int phrase3 = 9;
+    int phrase4 = 6;
+
+    tree.insert(phrase);
+    tree.insert(phrase1);
+    tree.insert(phrase2);
+    tree.insert(phrase3);
+    tree.insert(phrase4);
+
+
+    tree.search(2);
+
+    tree.preorder();
 
     return 0;
 }

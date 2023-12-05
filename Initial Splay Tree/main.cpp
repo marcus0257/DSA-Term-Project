11682610deba7c0e65915222348ed820082f3a43
Justin <133079600+Jusconn@users.noreply.github.com>
2023-12-04 19:05:50 -0500

    Update main.cpp

--- Initial Splay Tree/main.cpp
+++ Initial Splay Tree/main.cpp
@@ -1,25 +1,26 @@
-#include <iostream>
 #include "splay.h"
+#include <iostream>
 
-int main()
-{
+int main(){
+
     SplayTree tree;
-    int phrase = 1;
-    int phrase1 = 2;
-    int phrase2 = 3;
-    int phrase3 = 9;
-    int phrase4 = 6;
 
-    tree.insert(phrase);
-    tree.insert(phrase1);
-    tree.insert(phrase2);
-    tree.insert(phrase3);
-    tree.insert(phrase4);
+    for(int i = 0; i<15; i++){
+        tree.insert(i);
+    }
+    //preorder traversal prints the root of the tree first
+    tree.preorder();
 
+    //for every search, the searched node is splayed to the top of the tree
+    tree.search(7);
+    tree.search(8);
+    tree.search(4);
 
-    tree.search(2);
-
+    //root of tree should be 4 since it was the most recently searched, 7 and 8 should be realtively quick to access from 4,
+    // as they were very recently searched
     tree.preorder();
 
     return 0;
 }
+
+//feel free to play around with different search values and ranges for the for loop

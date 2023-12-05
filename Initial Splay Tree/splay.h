1884fad122c4d44576ca483fbb3cc8147eac297a
Justin <133079600+Jusconn@users.noreply.github.com>
2023-12-04 21:23:34 -0500

    Update splay.h

--- Initial Splay Tree/splay.h
+++ Initial Splay Tree/splay.h
@@ -8,13 +8,13 @@
 
 class SplayNode {
 private:
-    int key;  // Change from std::string to int
+    int key;  
     SplayNode* left;
     SplayNode* right;
 
 public:
     SplayNode();
-    SplayNode(int key);  // Change from std::string to int
+    SplayNode(int key);  
     ~SplayNode();
 
     friend class SplayTree;
@@ -25,22 +25,22 @@ private:
     SplayNode* root;
     SplayNode* rotateLeft(SplayNode* root);
     SplayNode* rotateRight(SplayNode* root);
-    SplayNode* splay(int key, SplayNode* root);  // Change from std::string to int
-    SplayNode* insert(int key, SplayNode* root);  // Change from std::string to int
-    SplayNode* remove(int key, SplayNode* root);  // Change from std::string to int
+    SplayNode* splay(int key, SplayNode* root);  
+    SplayNode* insert(int key, SplayNode* root); 
+    SplayNode* remove(int key, SplayNode* root);  
     void preorder(SplayNode* root);
     void inorder(SplayNode* root);
     void postorder(SplayNode* root);
     //void destroy(SplayNode* root);
-    SplayNode* search(int key, SplayNode* root);  // Change from std::string to int
-    SplayNode* New_Node(int key);  // Change from std::string to int
+    SplayNode* search(int key, SplayNode* root); 
+    SplayNode* New_Node(int key);  
 
 public:
     SplayTree();
     ~SplayTree();
 
-    void insert(int key);  // Change from std::string to int
-    void remove(int key);  // Change from std::string to int
+    void insert(int key);  
+    void remove(int key);  
     void preorder();
     void inorder();
     void postorder();
